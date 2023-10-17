0392e3f5e3b8b78deaed73442d40bb8cb6f00568
Rico Ronaldo <91813137+rico-r@users.noreply.github.com>
2023-10-17 09:46:19 +0700

    Fix update rule again

--- update.py
+++ update.py
@@ -1,56 +1,31 @@
 #! /bin/python3
 
 import os
-from datetime import datetime, timedelta
 import pandas as pd
 import yfinance as yf
 
-def formatDate(x: datetime):
-	return x.strftime("%Y-%m-%d")
-
-def add2date(start: str, days: int):
-	return formatDate(datetime.fromisoformat(start) + timedelta(days))
-
-def getCurrentDate():
-    return formatDate(datetime.now() + timedelta(days=1))
-
-def update(output: str, data: pd.DataFrame, emiten: str, startDate: str):
-    newData = yf.download(emiten + ".JK", progress=False, start=startDate)
-    if newData.index.size == 0:
-        print(f"Skipping {emiten}...")
-        return
-    data = data.drop(data.index[-1])
-    data = data.set_index('Date')
-    data.to_csv(output)
-    newData.to_csv(output, mode='a', header=False)
-
 def append(output: str, emiten: str):
-    data = pd.read_csv(output)
-
-    lastDate = data.Date[data.index[-1]]
-    nextDate = add2date(lastDate, 1)
-    if nextDate == endDate:
-        # update today data
-        print(f"Refresh {emiten}...")
-        return update(output, data, emiten, lastDate)
+    data = pd.read_csv(output).set_index('Date')
+    data.index = pd.to_datetime(data.index)
 
     print(f"Updating {emiten}...")
-    data = yf.download(emiten + ".JK", progress=False, start=nextDate, end=endDate)
+    newData = yf.download(emiten + ".JK", progress=False, start=data.index[-1], prepost=True)
 
     # either network problem or emiten is delisted
     if data.index.size == 0:
         print(f"Skipping {emiten}... due to no data")
         return
-    elif formatDate(data.index[0]) == lastDate:
-        print(f"Skipping {emiten}... already has latest data")
-        return
+
+    # update old data
+    data.update(newData)
+    data = data.combine_first(newData)
 
     # save result
-    data.to_csv(output, mode='a', header=False)
+    data.to_csv(output)
 
 def download(output: str, emiten: str):
     print(f"Downloading {emiten}...")
-    data = yf.download(emiten + ".JK", progress=False)
+    data = yf.download(emiten + ".JK", progress=False, prepost=True)
     if data.index.size == 0:
         print(f"Skipping {emiten}... due to no data")
         return
@@ -61,7 +36,6 @@ if len(os.sys.argv) > 1:
 else:
     emitenList = pd.read_csv('emiten.csv').emiten
 
-endDate = getCurrentDate()
 for emiten in emitenList:
     fileOutput = f"data/{emiten.lower()}.csv"
     if os.path.exists(fileOutput):
